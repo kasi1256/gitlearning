90d0dba0e9bf088fe02f46ae0e441491f607a6bc
kasi1256 <gundapaneedi_kasiviswanath@columbusstate.com>
2024-09-26 16:31:14 -0400

    initial push

---
+++ src/main/java/com/example/testing/gitlearning/GitlearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.testing.gitlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testing/gitlearning/GitlearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testing.gitlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
